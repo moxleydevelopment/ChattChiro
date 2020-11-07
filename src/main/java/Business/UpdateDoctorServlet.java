95f17227af52f26b0b83bd46b457119bca659965
Tim Wolf <timwolf1@gmail.com>
2020-11-07 11:52:01 -0500

    Adding servlet to handle updating doctor info

---
+++ src/main/java/Business/UpdateDoctorServlet.java
@@ -0,0 +1,122 @@
+package Business;
+
+/**
+ * Nicholas Hall, Timothy Wolf, Donya Moxley, Jason Fleurival, Benjamin Ard
+ * CIST2931
+ * Team 3
+ * ChattChiro - Chiropractors
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author donyamoxley
+ */
+@WebServlet(urlPatterns = {"/UpdateDoctorServlet"})
+public class UpdateDoctorServlet extends HttpServlet {
+    
+    
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request  servlet request
+     * @param response servlet response
+     * @throws ServletException                 if a servlet-specific error occurs
+     * @throws IOException                      if an I/O error occurs
+     * @throws java.sql.SQLException
+     * @throws java.lang.ClassNotFoundException
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException, ClassNotFoundException, Exception {
+        response.setContentType("text/html;charset=UTF-8");
+        String id, password, fName, lName, email, officeNum;
+        
+        try (PrintWriter out = response.getWriter()) {
+           
+            id = request.getParameter("chiroId"); 
+            password =  request.getParameter("chiroPwd");
+            fName = request.getParameter("chiroFName");
+            lName = request.getParameter("chiroLName");
+            email = request.getParameter("chiroEmail");
+            officeNum = request.getParameter("chiroOfficeNum");
+            
+            Doctor updatedDoc = new Doctor(id, password, fName, lName, email, officeNum);
+            updatedDoc.updateDB();
+            HttpSession ses1 = request.getSession();
+            ses1.setAttribute("d1", updatedDoc);
+            RequestDispatcher rd = request.getRequestDispatcher("/chiropractors/chiropractor.jsp");
+            rd.forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(PatientLoginServletDB.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(PatientLoginServletDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(PatientLoginServletDB.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(PatientLoginServletDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
