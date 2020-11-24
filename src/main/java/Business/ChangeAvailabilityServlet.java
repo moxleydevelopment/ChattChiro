39c217fab6f2963f2bc664bd5327a75542a62432
Donya Moxley <moxleydevelopment@gmail.com>
2020-11-24 14:41:12 -0500

    edit

--- src/main/java/Business/ChangeAvailabilityServlet.java
+++ src/main/java/Business/ChangeAvailabilityServlet.java
@@ -57,11 +57,11 @@ public class ChangeAvailabilityServlet extends HttpServlet {
                 String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
                
                 con = DriverManager.getConnection(dbUrl, username, dbPassword);
-                 System.out.println("DB URI: " + dbUrl);
+                
              } else {
                 
                  con = DriverManager.getConnection("jdbc:postgresql://localhost/postgres", "postgres", "root");
-                 System.out.println("DB URI: " + dbUrl);
+                
              }
             for (String input : request.getParameterMap().keySet()) {
                 for (String value : request.getParameterValues(input)) {
